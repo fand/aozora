1c35c600291c2fa5159e01960c2d6d51e104eed0
fand <fand@gmork.in>
2016-01-25 01:50:41 +0900

    Add findWorkById

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import fs from 'fs';
 import { parse } from 'csv';
 import p from '@fand/promisify';
 
-p(fs.readFile)('./list_person_all.utf8.csv')
-  .then((file) => p(parse, {})(file))
-  .then((data) => {
-    data.slice(0, 10).forEach((o) => console.log(o));
-  })
-  .catch((err) => console.error(err));
+let data;
+
+const init = function () {
+  if (data) {
+    return Promise.resolve();
+  }
+
+  return p(fs.readFile)('./list_person_all.utf8.csv')
+    .then((file) => p(parse, {})(file))
+    .then((_data) => data = _data)
+    .catch((err) => { throw err; });
+};
+
+export function findWorkById (workId) {
+  const workIdStr = `${workId}`;
+  return init().then(() => {
+    for (let i = 0, len = data.length; i < len; i++) {
+      if (workIdStr === data[i][2]) {
+        return data[i];
+      }
+    }
+  });
+}
