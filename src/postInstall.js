4bfa6946d4dfe91ba34cb2cc7347a89f9ef20b99
fand <fand@gmork.in>
2016-01-26 10:58:28 +0900

    Split Work schema to Work/Page

--- src/postInstall.js
+++ src/postInstall.js
@@ -32,22 +32,27 @@ const sequelize = new Sequelize('aozora', '', '', {
 // '出版社名'
 // '入力に使用した版'
 // '校正に使用した版'
+
+function rowToAuthor (row) {
+  return {
+    uuid  : row[0],
+    title : row[1],
+  };
+}
+
 function rowToWork (row) {
   return {
-    authorId         : row[0],
-    authorName       : row[1],
-    uuid             : row[2],
-    title            : row[3],
-    kanaType         : row[4],
-    translaterName   : row[5],
-    inputterName     : row[6],
-    proofreaderName  : row[7],
-    state            : row[8],
-    stateDate        : row[9],
-    originalBook     : row[10],
-    publisherName    : row[11],
-    inputEdition     : row[12],
-    proofreadEdition : row[13],
+    uuid  : row[2],
+    title : row[3],
+  };
+}
+
+function rowToPage (row) {
+  return {
+    authorId       : row[0],
+    workId         : row[2],
+    kanaType       : row[4],
+    translaterName : row[5],
   };
 }
 
@@ -56,25 +61,57 @@ const Author = sequelize.define('author', {
   name : Sequelize.STRING,
 });
 
-const Work = sequelize.define('work', {
-  uuid           : { type : Sequelize.UUID, primaryKey : true },
-  title          : Sequelize.STRING,
+const Work = sequelize.define('Work', {
+  uuid : { type : Sequelize.UUID, primaryKey : true },
+  title : Sequelize.STRING,
+});
+
+const Page = sequelize.define('page', {
+  workId : {
+    type : Sequelize.UUID,
+    references : {
+      model : Work,
+      key   : 'uuid',
+    },
+  },
+  authorId : {
+    type       : Sequelize.UUID,
+    references : {
+      model : Author,
+      key   : 'uuid',
+    },
+  },
   kanaType       : Sequelize.ENUM('新字新仮名', '新字旧仮名', '旧字新仮名', '旧字旧仮名', 'その他'),
   translaterName : Sequelize.STRING,
 });
 
-Promise.all([readCSV(), Author.sync(), Work.sync()]).then(() => {
+Promise.all([readCSV(), Author.sync(), Work.sync(), Page.sync()]).then(() => {
   const authorIds = {};
-  return data.reduce((prev, [uuid, name]) => prev.then(() => {
-    if (authorIds[uuid]) { return true; }
+  return data.reduce((prev, row) => prev.then(() => {
+    const { uuid, name } = rowToAuthor(row);
 
+    if (authorIds[uuid]) { return true; }
     authorIds[uuid] = true;
-    return Author.create({uuid, name});
+
+    return Author.create({ uuid, name });
+  }), Promise.resolve());
+})
+.then(() => {
+  const workIds = {};
+  return data.reduce((prev, row) => prev.then(() => {
+    const { uuid, title } = rowToWork(row);
+
+    if (workIds[uuid]) { return true; }
+    workIds[uuid] = true;
+
+    return Work.create({ uuid, title });
   }), Promise.resolve());
 })
 .then(() => {
   return data.reduce((prev, row) => prev.then(() => {
-    return Work.create(rowToWork(row));
+    const { workId, authorId, kanaType, translaterName } = rowToPage(row);
+    return Page.create({ workId, authorId, kanaType, translaterName })
+      .catch(e => console.log(e));
   }), Promise.resolve());
 })
 .then(() => {
