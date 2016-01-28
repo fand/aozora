e166ff0297fca388ff9b0d097a052b322291a389
fand <fand@gmork.in>
2016-01-28 22:10:49 +0900

    Find authors from CLI options

--- src/index.js
+++ src/index.js
@@ -19,23 +19,56 @@ const init = function () {
   return Promise.all([Author.sync(), Work.sync(), Card.sync()]);
 };
 
+function showAuthorById (authorId) {
+  return Author.find({ where : { uuid : authorId } })
+    .then((author) => {
+      console.log(author.get('name'));
+    })
+    .catch((e) => {
+      console.log('>>>>>>>>>error');
+      console.log(e);
+    });
+}
+
+function showAuthorByName (authorName) {
+  return Author.findAll({ where : { name : { $like : `%${authorName}%` } } })
+    .then((authors) => {
+      if (authors.length === 0) {
+        return console.log('No author found');
+      }
+
+      authors.forEach(a => console.log(a.get('name')));
+    })
+    .catch((e) => {
+      console.log('>>>>>>>>>error');
+      console.log(e);
+    });
+}
+
 /**
  * @param {string} authorIdOrName
  */
 export function showAuthor (authorIdOrName) {
-  console.log(`showAuthor : ${authorIdOrName}`);
-
-  const authorIdPadded = padStart(authorIdOrName, 6, '0');
-
   init().then(() => {
-    return Card.findAll({ where : { authorIdOrName } });
-  })
-  .then((cards) => {
-    cards.forEach((card) => {
-      const url = `http://www.aozora.gr.jp/cards/${authorIdPadded}/card${card.workId}.html`;
-      console.log(url);
-    });
+    if (/^\d+$/.test(authorIdOrName)) {
+      return showAuthorById(authorIdOrName);
+    }
+    else {
+      return showAuthorByName(authorIdOrName);
+    }
   });
+
+  // const authorIdPadded = padStart(authorIdOrName, 6, '0');
+  //
+  // init().then(() => {
+  //   return Card.findAll({ where : { authorId : authorIdOrName } });
+  // })
+  // .then((cards) => {
+  //   cards.forEach((card) => {
+  //     const url = `http://www.aozora.gr.jp/cards/${authorIdPadded}/card${card.workId}.html`;
+  //     console.log(url);
+  //   });
+  // });
 
 }
 
