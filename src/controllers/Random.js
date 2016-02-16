8cff135aeb7efba83089664e50da6de1fde3b52a
fand <fand@gmork.in>
2016-02-16 14:41:20 +0900

    Fix showRandom

--- src/controllers/Random.js
+++ src/controllers/Random.js
@@ -5,7 +5,7 @@ import * as View    from '../View';
 /**
  * @param {Number} length
  */
-export function random (length) {
+export function showRandom (length) {
   return Works.getRandomWork()
     .then(work => Fetcher.fetchTextByWorkId(work.get('uuid')))
     .then(text => View.showWorkText(text, length));
